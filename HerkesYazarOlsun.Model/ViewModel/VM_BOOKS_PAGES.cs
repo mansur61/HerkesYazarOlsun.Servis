6229e5c9a9437d11de0ef69f7db9ff3b1f65d381
mansuremin.kaya@mapeg.gov.tr <mansuremin.kaya@mapeg.gov.tr>
2024-03-17 23:12:29 +0300

    düzenlemeler iletildi.

--- HerkesYazarOlsun.Model/ViewModel/VM_BOOKS_PAGES.cs
+++ HerkesYazarOlsun.Model/ViewModel/VM_BOOKS_PAGES.cs
@@ -9,8 +9,8 @@ namespace HerkesYazarOlsun.Model.ViewModel
     public class VM_BOOKS_PAGES
     {
         public long ID { get; set; }
-        public string PageWrite { get; set; }
-        public string PageFoto { get; set; }
+        public string? PageWrite { get; set; }
+        public string? PageFoto { get; set; }
 
         // Kitap veritabanı id bilgisi
         public long BooksId { get; set; }
